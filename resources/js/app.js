75fda6a574883a4fd98bd6cad7ac3ccef11610c1
Mertcan <mertcanureten@gmail.com>
2022-01-24 18:47:22 +0300

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,21 @@
+require('./bootstrap');
+require('datatables.net');
+import $ from "jquery"
+
+$(function () {
+    var ajax_url = "http://localhost:8080/getCustomers";
+    $('#customers').DataTable({
+      ajax: {
+        url: ajax_url,
+        dataSrc: ''
+      },
+      columns: [
+        { "data" : "id" },
+        { "data" : "full_name" },
+        { "data" : "email" },
+        { "data" : "password" },
+        { "data" : "is_deleted" }
+           ]
+    });
+
+});
---
+++ resources/views/customers.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="author" content="Mertcan ÜRETEN">
+    <title>Vidizayn App</title>
+    <script src="{{ mix('js/app.js') }}"></script>
+    <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
+  </head>
+  <body>
+    <table id="customers"></table>
+  </body>
+</html>
